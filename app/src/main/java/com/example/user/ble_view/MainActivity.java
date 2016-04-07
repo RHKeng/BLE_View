0391e7bd58cd1fb21d0986342009a37576698582
RHKeng <458271503@qq.com>
2016-04-08 00:44:15 +0800

    It is a new version which is relized by the "Intent"!

--- app/src/main/java/com/example/user/ble_view/MainActivity.java
+++ app/src/main/java/com/example/user/ble_view/MainActivity.java
@@ -1,58 +1,22 @@
 package com.example.user.ble_view;
 
 import android.app.Activity;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.Paint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.DisplayMetrics;
-import android.view.Display;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class MainActivity extends Activity implements android.view.View.OnClickListener {
 
-    private int count = 0;
-    private int POINT_Size;
-    private int pointX = 380;
-    private int pointY = 100;
-    private int first = 0;
-    private boolean ShowMap = false;
-    private boolean Back = false;
-    private int MoveType = 1;    //定义区分移动的类型
-    private boolean Index = true;  //判断是否发生移动
-    private int xSpeed = 7;
-    private int ySpeed = 7;
     private ViewPager mViewPager;
     private PagerAdapter mPagerAdapter;
     private List<View> mViews = new ArrayList<View>();
@@ -71,137 +35,20 @@ public class MainActivity extends Activity implements android.view.View.OnClickL
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        //获取窗口管理器
-        WindowManager windowManager = getWindowManager();
-        Display display = windowManager.getDefaultDisplay();
-        DisplayMetrics metrics = new DisplayMetrics();
-        display.getMetrics(metrics);
-
         setContentView(R.layout.activity_main);
         initView();
         initViewPage();
         initEvent();
-        final MapView mapView = new MapView(this);
-
-        POINT_Size = 10;
-
-        final Handler handler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                if (msg.what == 0x123) {
-                    mapView.invalidate();
-                    if(Back==true)
-                    {
-                        if(first==0)
-                        {
-                            setContentView(R.layout.activity_main);
-                            first++;
-                        }
-                        ShowMap=false;
-                    }
-                    if (ShowMap==true)
-                    {
-                        setContentView(mapView);
-                        Back=false;
-                    }
-                }
-            }
-        };
-
-        mapView.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event)
-            {
-                switch (event.getKeyCode())
-                {
-                    case KeyEvent.KEYCODE_B:
-                    {
-                        Toast.makeText(MainActivity.this,"get1",Toast.LENGTH_SHORT).show();
-                        Back = true;
-                        break;
-                    }
-                    case KeyEvent.KEYCODE_D:
-                    {
-                        Toast.makeText(MainActivity.this,"get2",Toast.LENGTH_SHORT).show();
-                        break;
-                    }
-                }
-                return true;
-            }
-        });
-
-        final Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                if (Index == true) {
-                    if (count == 18) {
-                        count = 0;
-                        if (MoveType == 4) {
-                            Index = false;
-                        }
-                        MoveType = 4;
-                        //Index = false;
-                    } else {
-                        count++;
-                        switch (MoveType) {
-                            case 1:  //向左运动
-                            {
-                                pointX = pointX - xSpeed;
-                                break;
-                            }
-                            case 2:  //向右运动
-                            {
-                                pointX = pointX + xSpeed;
-                                break;
-                            }
-                            case 3:  //向上运动
-                            {
-                                pointY = pointY - ySpeed;
-                                break;
-                            }
-                            case 4:  //向下运动
-                            {
-                                pointY = pointY + ySpeed;
-                                break;
-                            }
-                        }
-                    }
-                } else {
-
-                }
-                handler.sendEmptyMessage(0x123);
-            }
-        }, 0, 200);
-
     }
 
-    class MapView extends View {
 
-        Paint paint = new Paint();
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.map);
-        Matrix matrix = new Matrix();
-
-        public MapView(Context context) {
-            super(context);
-            setFocusable(true);
-        }
-
-        public void onDraw(Canvas canvas) {
-            matrix.setScale(0.6f, 1.0f);
-            paint.setStyle(Paint.Style.FILL);
-            canvas.drawBitmap(bitmap, matrix, null);
-            paint.setColor(Color.rgb(255, 0, 0));
-            canvas.drawCircle(pointX, pointY, POINT_Size, paint);
-        }
-    }
 
 
     public void MapButtonClick(View view)
     {
         Toast.makeText(MainActivity.this,"get",Toast.LENGTH_SHORT).show();
-        ShowMap=true;
-
+        Intent intent= new Intent(MainActivity.this,MapActivity.class);
+        startActivity(intent);
     }
 
     private void initEvent() {
---
+++ app/src/main/java/com/example/user/ble_view/MapActivity.java
@@ -0,0 +1,145 @@
+package com.example.user.ble_view;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by keng on 2016/4/7.
+ */
+public class MapActivity extends MainActivity{
+
+    private int count = 0;
+    private int POINT_Size;
+    private int pointX = 380;
+    private int pointY = 100;
+    private int MoveType = 1;    //定义区分移动的类型
+    private boolean Index = true;  //判断是否发生移动
+    private int xSpeed = 7;
+    private int ySpeed = 7;
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        //获取窗口管理器
+        WindowManager windowManager = getWindowManager();
+        Display display = windowManager.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+
+        final MapView mapView = new MapView(this);
+
+        POINT_Size = 10;
+
+        setContentView(mapView);
+
+        final Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == 0x123) {
+                    mapView.invalidate();
+                }
+            }
+        };
+
+        mapView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                switch (event.getKeyCode()) {
+                    case KeyEvent.KEYCODE_B: {
+                        Intent intent = new Intent(MapActivity.this,MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                        break;
+                    }
+                    case KeyEvent.KEYCODE_D: {
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
+
+        final Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (Index == true) {
+                    if (count == 18) {
+                        count = 0;
+                        if (MoveType == 4) {
+                            Index = false;
+                        }
+                        MoveType = 4;
+                        //Index = false;
+                    } else {
+                        count++;
+                        switch (MoveType) {
+                            case 1:  //向左运动
+                            {
+                                pointX = pointX - xSpeed;
+                                break;
+                            }
+                            case 2:  //向右运动
+                            {
+                                pointX = pointX + xSpeed;
+                                break;
+                            }
+                            case 3:  //向上运动
+                            {
+                                pointY = pointY - ySpeed;
+                                break;
+                            }
+                            case 4:  //向下运动
+                            {
+                                pointY = pointY + ySpeed;
+                                break;
+                            }
+                        }
+                    }
+                } else {
+
+                }
+                handler.sendEmptyMessage(0x123);
+            }
+        }, 0, 200);
+
+    }
+
+    class MapView extends View {
+
+        Paint paint = new Paint();
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.map);
+        Matrix matrix = new Matrix();
+
+        public MapView(Context context) {
+            super(context);
+            setFocusable(true);
+        }
+
+        public void onDraw(Canvas canvas) {
+            matrix.setScale(0.6f, 1.0f);
+            paint.setStyle(Paint.Style.FILL);
+            canvas.drawBitmap(bitmap, matrix, null);
+            paint.setColor(Color.rgb(255, 0, 0));
+            canvas.drawCircle(pointX, pointY, POINT_Size, paint);
+        }
+    }
+}
